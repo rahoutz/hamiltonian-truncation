85f1bc9614e40971a25ee0e802153c1b5b19f05a
rahoutz <13279883+rahoutz@users.noreply.github.com>
2021-09-15 19:36:41 +0100

    Add files via upload

--- hamiltonian-truncation.py
+++ hamiltonian-truncation.py
@@ -1,10 +1,14 @@
 # #################################################################################################################
-# ######                                                                                 ###########################
-# ######                                                                                 ###########################
-# ######                                                                                 ###########################
-# ######                                                                                 ###########################
-# ######                                                                                 ###########################
-# ######                                                                                 ###########################
+# ######                                                                                        ####################
+# ######        Hamiltonain Truncation                                                          ####################
+# ######        Authors: Rachel Houtz, Kara Farnsworth, Markus Luty                             ####################
+# ######                                                                                        ####################
+# ######        This python code can be used to quickly generate the truncated Hamiltonian      ####################
+# ######        for 2d \phi^4 theory. The interacting Hamiltonian matrix in a basis truncated   ####################
+# ######        at ~10^6 states can be generated on O(days) using this code with modest         ####################
+# ######        computational resources (for example, a laptop). See the accompanying           ####################
+# ######        work arXiv:2109.XXXX.                                                           ####################
+# ######                                                                                        ####################
 # ##################################################################################################################
 import math
 import numpy as np
