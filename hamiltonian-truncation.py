ea811eb0da1e5a0e650bcd21092b42e3304bdd9c
rahoutz <13279883+rahoutz@users.noreply.github.com>
2021-10-19 10:50:28 +0200

    Update hamiltonian-truncation.py

--- hamiltonian-truncation.py
+++ hamiltonian-truncation.py
@@ -1,13 +1,13 @@
 # #################################################################################################################
 # ######                                                                                        ####################
 # ######        Hamiltonain Truncation                                                          ####################
-# ######        Authors: Rachel Houtz, Kara Farnsworth, Markus Luty                             ####################
+# ######        Authors: Rachel Houtz, Tim Cohen, Kara Farnsworth, Markus Luty                  ####################
 # ######                                                                                        ####################
 # ######        This python code can be used to quickly generate the truncated Hamiltonian      ####################
 # ######        for 2d \phi^4 theory. The interacting Hamiltonian matrix in a basis truncated   ####################
 # ######        at ~10^6 states can be generated on O(days) using this code with modest         ####################
 # ######        computational resources (for example, a laptop). See the accompanying           ####################
-# ######        work arXiv:2109.XXXX.                                                           ####################
+# ######        work arXiv:2110.08273.                                                          ####################
 # ######                                                                                        ####################
 # ##################################################################################################################
 import math
